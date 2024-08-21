f279b0733858ddd9bd4ea8e11f8db62d5940ce58
Sebastian-Sanchez-Caro <164094988+Sebastian-Sanchez-Caro@users.noreply.github.com>
2024-08-21 18:57:46 -0500

    tierro

---
+++ main.js
@@ -0,0 +1,18 @@
+function generarTabla(){
+    let multiplicando = document.getElementById('contador').value;
+
+    document.getElementById('x1').innerText = multiplicando + " x 1  = " + multiplicando*1;
+    document.getElementById('x2').innerText = multiplicando + " x 2  = " + multiplicando*2;
+    document.getElementById('x3').innerText = multiplicando + " x 3  = " + multiplicando*3;
+    document.getElementById('x4').innerText = multiplicando + " x 4  = " + multiplicando*4;
+    document.getElementById('x5').innerText = multiplicando + " x 5  = " + multiplicando*5;
+    document.getElementById('x6').innerText = multiplicando + " x 6  = " + multiplicando*6;
+    document.getElementById('x7').innerText = multiplicando + " x 7  = " + multiplicando*7;
+    document.getElementById('x8').innerText = multiplicando + " x 8  = " + multiplicando*8;
+    document.getElementById('x9').innerText = multiplicando + " x 9  = " + multiplicando*9;
+    document.getElementById('x10').innerText = multiplicando + " x 10  = " + multiplicando*10;
+
+    
+    
+
+}
